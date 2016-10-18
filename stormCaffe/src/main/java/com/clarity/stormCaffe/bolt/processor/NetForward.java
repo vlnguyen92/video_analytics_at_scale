f38d91686cfd9dd29bb2396ab48439a3a1a32888
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-18 15:46:40 -0400

    Finish NetForward processor

--- stormCaffe/src/main/java/com/clarity/stormCaffe/bolt/processor/NetForward.java
+++ stormCaffe/src/main/java/com/clarity/stormCaffe/bolt/processor/NetForward.java
@@ -2,8 +2,14 @@ package com.clarity.stormCaffe.bolt.processor;
 
 import com.clarity.stormCaffe.model.Frame;
 import org.apache.storm.task.TopologyContext;
+import org.bytedeco.javacpp.FloatPointer;
 import org.bytedeco.javacpp.caffeC3DOverlapLoss;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_imgproc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,17 +18,22 @@ import java.util.Map;
 public class NetForward extends FrameProcessor {
     String model;
     String weight;
-    String mean;
-    String outputLayer;
+    String outputBlobName;
     boolean useGPU;
 
     caffeC3DOverlapLoss.FloatNet net;
+    caffeC3DOverlapLoss.FloatBlob inputBlob;
+    caffeC3DOverlapLoss.FloatBlob outputBlob;
+    opencv_core.Size inputGeometry;
 
-    public NetForward(String model, String weight, String mean, String outputLayer, boolean useGPU) {
+    public NetForward(String model, String weight, boolean useGPU) {
+        this(model, weight, "", useGPU);
+    }
+
+    public NetForward(String model, String weight, String outputLayer, boolean useGPU) {
         this.model = model;
         this.weight = weight;
-        this.mean = mean;
-        this.outputLayer = outputLayer;
+        this.outputBlobName = outputLayer;
         this.useGPU = useGPU;
     }
 
@@ -37,11 +48,101 @@ public class NetForward extends FrameProcessor {
             throw new IllegalArgumentException("Network should have exactly 1 input");
         }
 
-        caffeC3DOverlapLoss.FloatBlob input_blob = net.input_blobs().get(0);
+        inputBlob = net.input_blobs().get(0);
+
+        if (outputBlobName.isEmpty()) {
+            outputBlob = net.output_blobs().get(0);
+        } else {
+            if (!net.has_blob(outputBlobName)) {
+                throw new IllegalArgumentException("The network doesn't have a blob named " + outputBlobName);
+            }
+            outputBlob = net.blob_by_name(outputBlobName);
+        }
+
+        if (inputBlob.channels() != 3 && inputBlob.channels() != 1) {
+            throw new IllegalArgumentException("Input layer should have 1 or 3 channels");
+        }
+        inputGeometry = new opencv_core.Size(inputBlob.width(), inputBlob.height());
     }
 
     @Override
     public Frame execute(Frame input) throws Exception {
+        HashMap<String, Object> meta = input.getMetadata();
+        meta.put(outputBlobName, forward(input.getImage()));
         return input;
+    }
+
+    private List<opencv_core.Mat> forward(opencv_core.Mat img) {
+        // resize to handle one image a time, change this when we are ready for batch processing
+        inputBlob.Reshape(1, inputBlob.channels(), inputBlob.height(), inputBlob.width());
+
+        preprocess(img, inputBlobAsMats(0));
+        net.ForwardPrefilled();
+
+        int height = outputBlob.height();
+        int width = outputBlob.width();
+        int channels = outputBlob.channels();
+        FloatPointer outputData = outputBlob.cpu_data();
+        List<opencv_core.Mat> listMats = new ArrayList<>();
+        for (int i = 0; i != outputBlob.num(); ++i) {
+            FloatPointer curr = outputData.position(outputBlob.offset(i));
+            // no copy here, we will copy data when construct CVMat serializer
+            listMats.add(new opencv_core.Mat(height, width, opencv_core.CV_32FC(channels), curr));
+        }
+        return listMats;
+    }
+
+    /**
+     * Wrap input blob as a vector of opencv.Mat representing each channel.
+     * @param n which image in blob to wrap
+     * @return the wrapped vector of Mat
+     */
+    private opencv_core.MatVector inputBlobAsMats(int n) {
+        int channels = inputBlob.channels();
+        int height = inputBlob.height();
+        int width = inputBlob.width();
+
+        FloatPointer inputData = inputBlob.mutable_cpu_data();
+        opencv_core.MatVector vecMat = new opencv_core.MatVector();
+        vecMat.resize(inputBlob.channels());
+
+        for (int c = 0; c != channels; ++c) {
+            FloatPointer curr = inputData.position(inputBlob.offset(n, c));
+            opencv_core.Mat mat = new opencv_core.Mat(height, width, opencv_core.CV_32FC1, curr);
+            vecMat.put(mat);
+        }
+        return vecMat;
+    }
+
+    /**
+     * Preprocess the image so it's suitable for the network, write output to outputChannels
+     * @param img image to process
+     * @param outputChannels outputChannels
+     * @return the outputChannels
+     */
+    private opencv_core.MatVector preprocess(opencv_core.Mat img, opencv_core.MatVector outputChannels) {
+        if (img.channels() == 3 && outputChannels.size() == 1) {
+            opencv_imgproc.cvtColor(img, img, opencv_imgproc.COLOR_BGR2GRAY);
+        } else if (img.channels() == 4 && outputChannels.size() == 1) {
+            opencv_imgproc.cvtColor(img, img, opencv_imgproc.COLOR_BGRA2GRAY);
+        }  else if (img.channels() == 4 && outputChannels.size() == 3) {
+            opencv_imgproc.cvtColor(img, img, opencv_imgproc.COLOR_BGRA2BGR);
+        } else if (img.channels() == 1 && outputChannels.size() == 3) {
+            opencv_imgproc.cvtColor(img, img, opencv_imgproc.COLOR_GRAY2BGR);
+        }
+
+        if (img.size() != inputGeometry) {
+            opencv_imgproc.resize(img, img, inputGeometry);
+        }
+
+        opencv_core.Mat img_float = new opencv_core.Mat();
+        if (outputChannels.size() == 3) {
+            img.convertTo(img_float, opencv_core.CV_32FC3);
+        } else {
+            img.convertTo(img_float, opencv_core.CV_32FC1);
+        }
+
+        opencv_core.split(img_float, outputChannels);
+        return outputChannels;
     }
 }
