641a475027343787f86926ebbd369a31158f1d16
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 19:51:25 -0400

    Fix sourcing helpers

--- serverConfig/scripts/compile-javacpp.sh
+++ serverConfig/scripts/compile-javacpp.sh
@@ -5,6 +5,8 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 
+source "$__dir/helpers.sh"
+
 CORE_NUMBER=$(grep -c ^processor /proc/cpuinfo)
 
 ## Javacpp presets
