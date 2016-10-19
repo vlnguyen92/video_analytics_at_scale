9e2adab1999b0f5386d18614bf38e04e374c59d4
Aetf <aetf@unlimitedcodeworks.xyz>
2016-10-19 19:49:56 -0400

    Fix typo

--- serverConfig/scripts/compile-javacpp.sh
+++ serverConfig/scripts/compile-javacpp.sh
@@ -19,7 +19,7 @@ git clone https://github.com/Aetf/javacpp-presets.git
 sudo docker run -i --privileged -v /home/cc/buildbed:/usr/local/buildbed -v /usr/local/cuda:/usr/local/cuda -v /home/cc/.m2:/root/.m2 centos:7 /bin/bash <<EOF
 yum -y install epel-release
 yum -y install clang gcc-c++ gcc-gfortran java-devel maven python numpy swig git file which wget unzip tar bzip2 gzip xz patch make cmake perl nasm yasm atlas-devel openblas-devel freeglut-devel gtk2-devel libusb-devel libusb1-devel zlib-devel
-yum install \`rpm -qa | sed s/.x86_64$/.i686/\`
+yum -y install \`rpm -qa | sed s/.x86_64$/.i686/\`
 cd /usr/local/buildbed/javacpp-presets
 mvn clean install -Djavacpp.platform=linux-x86_64 -Djavacpp.platform.dependency=false --projects .,opencv,ffmpeg,caffe,caffeC3DOverlapLoss,caffeC3DSampleRate
 EOF
